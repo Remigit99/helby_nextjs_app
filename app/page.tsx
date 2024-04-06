028784f1b77bc2294682581443028600bc79f704
Remigit99 <aderemibiodun1000@gmail.com>
2024-04-06 18:20:47 +0100

    develop the header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,54 @@
 import Image from "next/image";
+import Link from "next/link";
+import style from "./page.module.css"
+
+import heroAvatar from "./assets/user_profile.png"
+import headerIllustration from "./assets/hero_illustration.png"
+
+
 
 export default function Home() {
   return (
-    <main >
-      <h2>Welcome Everyone!</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, vitae commodi ab tempore autem aspernatur delectus, eaque est corrupti nemo, iure laudantium. Alias fuga eaque facere, earum vitae quas dolores distinctio fugit, qui possimus saepe numquam corporis ullam inventore adipisci dolore ipsum vero, ad iure? Mollitia voluptate, pariatur quo perspiciatis quia tempore non. Voluptate vitae blanditiis possimus laudantium, corporis odio dignissimos ratione ipsam ducimus maxime modi mollitia? Magni rem unde dicta, dignissimos tempora quisquam ea minus in omnis eaque, esse animi aut cum. In cum tenetur rem, voluptatum tempora voluptas, sed possimus aspernatur vitae ullam, similique officia recusandae assumenda doloremque.
-      </p>
-    </main>
+    <main className={`container ${style.mainContainer}`}>
+
+      <header className={style.header}>
+        <div className={style.headerContainer}>
+          <div className={style.headerContainerLeft}>
+
+            <div className={style.headerTitle}>
+              <h1>Simplify your</h1>
+              <h1>customer acqusition</h1>
+              <h1>process</h1>
+            </div>
+
+            <p>
+              Powerful natural language processing capabilities, that can understand and
+              respond to customer inquries in real-time & improve customer satisfaction.
+            </p>
+
+            <div className={style.demoBtns}>
+              <Link href="/" className={style.demoBtnA}>Get started free</Link>
+              <Link href="/" className={style.demoBtnB}>Book a demo</Link>
+            </div>
+
+            <div className={style.heroSpecialist}>
+              <div className={style.heroSpecLeft}>
+                <Image src={heroAvatar} alt="hero Avatar" className={style.heroAvatar} />
+              </div>
+
+              <div className={style.heroSpecRight}>
+                <small>15 min delivery call</small>
+                <p>Talk with a <span>Specialist</span></p>
+              </div>
+            </div>
+          </div>
+          <div className={style.headerContainerRight}>
+            <div>
+              <Image src={headerIllustration} alt="" />
+            </div>
+          </div>
+        </div>
+      </header>
+    </ main >
   );
 }
