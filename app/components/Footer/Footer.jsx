a0fd0b96962f8406d7e6dd3daab5383a9a440455
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-05-19 16:29:59 +0100

    modify footer

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,10 +1,71 @@
+import Link from "next/link";
+import Image from "next/image";
 
+import { FaFacebook } from "react-icons/fa";
+import { IoLogoTwitter } from "react-icons/io5";
+import { LuInstagram } from "react-icons/lu";
+import { IoLogoYoutube } from "react-icons/io";
 
+import NavLogo from "../../assets/Logo_nav.svg";
 
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
-}
+  return (
+    <footer>
+      <div className="footerContainer">
+        <div className="footerContainerLeft">
+          <Link href="/">
+            <Image src={NavLogo} alt="Helby_Logo" />
+          </Link>
+        </div>
 
-export default Footer
+        <div className="companyContacts">
+          <div className="address">
+            <h6>Address:</h6>
+            <small>16 Balogun Estate, Lagos Nigeria</small>
+          </div>
+
+          <div className="phone">
+            <h6>Contact:</h6>
+            <p>+234894588668</p>
+            <a href="mailto:aderemibiodun@gmail.com">aderemibiodun@gmail.com</a>
+          </div>
+
+          <div className="socialLinks">
+            <a href="http://facebook.com">
+              <FaFacebook />
+            </a>
+
+            <a href="http://twitter.com">
+              <IoLogoTwitter />
+            </a>
+            <a href="http://instagram.com">
+              <LuInstagram />
+            </a>
+            <a href="http://youtube.com">
+              <IoLogoYoutube />
+            </a>
+          </div>
+        </div>
+
+        <div className="footerContainerRight">
+          <div>
+            <li>About Us</li>
+            <li> Book a Demo </li>
+            <li> Blog </li>
+            <li> Pricing </li>
+            <li> Contact </li>
+          </div>
+
+          <div>
+            <li>Features</li>
+            <li>Sign in</li>
+            <li>Sign Up</li>
+            <li>Forget Password</li>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
